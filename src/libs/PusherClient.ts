31ff12e7119cabdf494b7233f7af05625768b1fa
Wargas <teixeira.wargas@gmail.com>
2023-03-06 12:32:01 -0300

    f

--- src/libs/PusherClient.ts
+++ src/libs/PusherClient.ts
@@ -1,10 +1,11 @@
 import Pusher from 'pusher-js'
 
 export const PusherClient = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    forceTLS: false,
-    cluster: '',
-    disableStats: true,
-    enabledTransports: ['ws', 'wss']
+    // wsHost: import.meta.env.VITE_PUSHER_HOST,
+    // wsPort: import.meta.env.VITE_PUSHER_PORT,
+    // wssPort: import.meta.env.VITE_PUSHER_PORT,
+    forceTLS: true,
+    cluster: 'us2',
+    // disableStats: true,
+    // enabledTransports: ['ws', 'wss']
 });
